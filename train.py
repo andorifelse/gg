72912b3048f14b65685238c6add0e2c6705dda3e
andorifelse <445538301@qq.com>
2026-03-18 01:34:08 +0800

    v5_innner_iter

--- train.py
+++ train.py
@@ -180,7 +180,7 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
             loss_obj_2d = loss_obj_base + lambda_collapse * loss_collapse
         else:
             if iteration < inner_end_iter:
-                if iteration % 3 == 0:
+                if iteration % 5 == 0:
                     loss_inner = compute_interior_consistency_loss(
                         logits=logits,
                         gt_obj=gt_obj,
@@ -381,8 +381,8 @@ if __name__ == "__main__":
     parser.add_argument('--port', type=int, default=6009)
     parser.add_argument('--debug_from', type=int, default=-1)
     parser.add_argument('--detect_anomaly', action='store_true', default=False)
-    parser.add_argument("--test_iterations", nargs="+", type=int, default=[7_000, 30_000, 30_000])
-    parser.add_argument("--save_iterations", nargs="+", type=int, default=[7_000, 30_000, 60_000])
+    parser.add_argument("--test_iterations", nargs="+", type=int, default=[7_000, 25_000, 28_000, 30_000, 30_000])
+    parser.add_argument("--save_iterations", nargs="+", type=int, default=[7_000, 25_000, 28_000, 30_000, 30_000])
     parser.add_argument("--quiet", action="store_true")
     parser.add_argument("--checkpoint_iterations", nargs="+", type=int, default=[])
     parser.add_argument("--start_checkpoint", type=str, default=None)
